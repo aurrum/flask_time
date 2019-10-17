a7bc77866c9f8904896b6a42511723ed571fce3b
Thangadurai Ponnusamy <thangadurai.ponnusamy@siemens.com>
2019-10-17 10:46:22 +0530

    updated

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ def homepage():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
     return """
-    <h1 align="center">Welcome to aurrum</h1>
-    <p align="center">It is currently {time}.</p>
+    <h1>Welcome to aurrum</h1>
+    <p>It is currently {time}.</p>
 
-    <img src="http://placeimg.com/700/500/tech" style=align:"center"/>
+    <img src="http://placeimg.com/700/500/tech"/>
     """.format(time=the_time)
 
 if __name__ == '__main__':
